a4b089a2ca3c498613dd8cb10bf45e74e31cd0a0
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-12-13 15:16:15 +0100

    Use node.status

--- src/mxd-search.js
+++ src/mxd-search.js
@@ -2,19 +2,24 @@ module.exports = (RED) => {
   RED.nodes.registerType('mxd-search', function NODE(config) {
     RED.nodes.createNode(this, config);
     const node = this;
+    node.status({});
 
     if (!config.limit) {
+      node.status({ fill: 'red', shape: 'dot', text: 'config is missing' });
       node.error('config is missing');
     }
-    node.log('initialize mxd-search node');
+    node.log(`initialize mxd-search node with an limit of ${config.limit}`);
 
     const { AssetsQuery, heimdall } = RED.nodes.getNode(config.heimdall);
 
     node.on('input', async (msg) => {
+      node.status({ fill: 'grey', shape: 'dot', text: `requesting...` });
       const search = msg.payload;
 
       if (!search.title) {
-        node.warn('no title in the payload');
+        node.status({ fill: 'yellow', shape: 'dot', text: 'no title in the payload' });
+        node.warn('no title in the payload, payload: ');
+        node.warn(search);
         return;
       }
 
@@ -22,9 +27,17 @@ module.exports = (RED) => {
         .filter('contentTypeSeriesOrMovies')
         .filter('search', search.title)
         .query('pageSize', config.limit);
-      const assets = await heimdall.getAssets(query);
+      let assets;
+      try {
+        assets = await heimdall.getAssets(query);
+      } catch (e) {
+        node.status({ fill: 'yellow', shape: 'dot', text: 'requesting error' });
+        node.warn(`requesting error (${e.message})`);
+        return;
+      }
 
+      node.status({});
       node.send({ payload: assets });
     });
-  }); 
+  });
 };
